6158eb720d893075e637054f3c6129a3349bf51e
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2014-05-14 20:05:11 +0200

    Initial Commit

---
+++ IoTWeek/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IoTWeek
+//
+//  Created by Thomas Gilbert on 14/05/14.
+//  Copyright (c) 2014 ITAdvice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
