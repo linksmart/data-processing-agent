2b0a662ac55e4ec9796006e6b8c60be2757174dd
Giorgio <daltoe@ismb.it>
2014-05-29 15:24:32 +0200

    new etsi m2m manager added

---
+++ etsi-m2m-manager/src/main/java/it/ismb/pertlab/pwal/etsi_m2m_manager/App.java
@@ -0,0 +1,13 @@
+package it.ismb.pertlab.pwal.etsi_m2m_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
