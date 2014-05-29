176506bc1269e75f42b82edf41ba99917b583de4
gbono <bono@ismb.it>
2014-05-29 15:13:50 +0200

    added pwal api data pusher interface and created cnet data pusher

---
+++ datapusher.cnet/src/main/java/it/ismb/pertlab/pwal/datapusher/cnet/CnetDataPusher.java
@@ -0,0 +1,18 @@
+package it.ismb.pertlab.pwal.datapusher.cnet;
+
+import it.ismb.pertlab.pwal.api.data.pusher.DataPusher;
+
+public class CnetDataPusher extends DataPusher 
+{
+
+	public CnetDataPusher(int seconds) {
+		super(seconds);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+   
+}
