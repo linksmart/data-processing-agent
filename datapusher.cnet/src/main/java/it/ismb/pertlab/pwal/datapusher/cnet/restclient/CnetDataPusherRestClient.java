efc7b9b2f23cb07dbd1114b392e784358d7fd0a0
gbono <bono@ismb.it>
2014-06-04 14:13:06 +0200

    added rest client for cnet

---
+++ datapusher.cnet/src/main/java/it/ismb/pertlab/pwal/datapusher/cnet/restclient/CnetDataPusherRestClient.java
@@ -0,0 +1,63 @@
+package it.ismb.pertlab.pwal.datapusher.cnet.restclient;
+
+import it.ismb.pertlab.pwal.datapusher.cnet.datamodel.ArrayOfIoTEntity;
+import it.ismb.pertlab.pwal.datapusher.cnet.datamodel.IoTEntity;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class CnetDataPusherRestClient {
+
+	private static Logger log = null;
+	private String serviceEndpoint;
+	private RestTemplate template;
+	
+	public CnetDataPusherRestClient(String endpoint, Logger logger)
+	{
+		this.template = new RestTemplate();
+		log = logger;
+		this.serviceEndpoint = endpoint;
+	}
+	
+	public ArrayOfIoTEntity alreadyExists(String deviceName)
+	{
+		HttpHeaders h=new HttpHeaders();
+		h.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
+		HttpEntity<ArrayOfIoTEntity> entity=new HttpEntity<ArrayOfIoTEntity>(h);
+		log.debug("Contacting {}?like={} ",serviceEndpoint,deviceName);
+		ResponseEntity<ArrayOfIoTEntity> r=template.exchange(serviceEndpoint+"?like=" + deviceName, HttpMethod.GET, entity, ArrayOfIoTEntity.class);
+		ArrayOfIoTEntity response = new ArrayOfIoTEntity();
+		if(r.getStatusCode().compareTo(HttpStatus.OK)==0)
+		{
+			response = (ArrayOfIoTEntity)r.getBody();
+			return response;
+		}
+		return null;
+	}
+	
+	public String pushNewIoTEntity(IoTEntity iotEntity)
+	{
+		HttpHeaders h=new HttpHeaders();
+		h.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
+		HttpEntity<IoTEntity> entity=new HttpEntity<IoTEntity>(iotEntity,h);
+		log.debug("Contacting {} ",serviceEndpoint);
+		ResponseEntity<IoTEntity> r=template.exchange(serviceEndpoint, HttpMethod.POST, entity, IoTEntity.class);
+		IoTEntity response = new IoTEntity();
+		if(r.getStatusCode().compareTo(HttpStatus.OK)==0)
+		{
+			response = (IoTEntity)r.getBody();
+			log.debug("Recevied xml response: ");
+			response.toXml();
+			return response.getAbout();
+		}
+		return null;
+	}
+}
