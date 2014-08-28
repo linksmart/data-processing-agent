8599d5ee27f592e5b8c513398ddd87e4ce7f66b9
Jose Angel <carvajal@fit.fhg.de>
2014-08-28 11:16:48 +0200

    first commit

---
+++ platform/Eventing/DataFusionAPI/src/main/java/eu/linksmart/api/event/datafusion/DataFusionWrapper.java
@@ -0,0 +1,82 @@
+package eu.linksmart.api.event.datafusion;
+
+import java.util.Map;
+/**
+ * 
+ * The DF wrapper is an Interface . The Interface provide API in which the several engines connect to the Almanac framework. 
+ * This API can be accessed natively in a OSGi environment or by Web Services,
+ *  allowing the a high decupled infrastructure in which any engine could be added so long implement a Wrapper which interact through Web Services. 
+ * 
+ * @author Jos� �ngel Carvajal Soto
+ * @version     0.01
+ * @since       0.01
+ * @see  DataFusionWrapper
+ * 
+ * */
+
+
+public interface DataFusionWrapper {
+
+	/**
+	 * Return the name of the CEP which implement the interface
+	 * 
+	 * @return Name of the CEP engine wrapped
+	 * 
+	 * */
+	public String getName();
+	/**
+	 * Add send and event to the CEP engine 
+	 * 
+	 * @param topic where the event was published
+	 * @param parts is the event itself. A map of values and values names
+	 * 
+	 * @return <code>true</code> if the event was added to the CEP engine. <code>false</code> otherwise.
+	 * */
+	public boolean addEvent(String topic, Map<String, Object> parts);
+	/**
+	 * Configure a particular type in the engine.
+	 * 
+	 *  This functionality is consider deprecated due to the unclearness of this concept as general concept for all DataFusionWrapper.<p>
+	 *  More research must be done about this to discard this feature or not. 
+	 * 
+	 * @param nameType are the names of type added in the engine
+	 * @param eventSchema are the names of the inner values of the event
+	 * @param eventTypes are the types of the inner values of the event
+	 * 
+	 * @return <code>true</code> if the event was added to the CEP engine. <code>false</code> otherwise.
+	 * */
+	@Deprecated
+	public boolean addEventType(String nameType, String[] eventSchema, Object[] eventTypes);
+	/**
+	 * Configure a particular type to a particular topic.<p>
+	 * More research must be done about this to discard this feature or not. 
+	 * 
+	 * @param topic to be associated whit a topic.
+	 * @param eventType is the pre-configured in the CEP engine type which will be associated with a topic. 
+	 * 
+	 * @return <code>true</code> if the topic was configurated to the CEP engine. <code>false</code> otherwise.
+	 * */
+	@Deprecated
+	public boolean setTopicToEventType(String topic, String eventType);
+	/**
+	 * Add a query to the CEP engine which would be later handler whith a handler.<p>
+	 * 
+	 * NOTE: currently there is no abstract data fusion language, therefore the query sent is a engine specific, this will change along with this function!
+	 * 
+	 * @param name which reference the query
+	 * @param query to be implemented in the CEP engine. <p>
+	 * @param topic which the query will query  
+	 * 
+	 * @return <code>true</code> if the query is successfully deployed in the CEP engine. <code>false</code> otherwise.
+	 * */
+	public boolean addQuery(String name, String query, String topic);
+	
+	/// Temporar
+	public boolean puseQuery(String name);
+
+	/// Temporar
+	public boolean startQuery(String name);
+
+	/// Temporar
+	public boolean removeQuery(String name);
+}
