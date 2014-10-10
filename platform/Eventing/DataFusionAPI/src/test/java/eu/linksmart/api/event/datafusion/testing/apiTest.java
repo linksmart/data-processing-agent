22e907b72b27a1ae0cb3f58983ee9c085d5256af
Jose Angel <carvajal@fit.fhg.de>
2014-10-10 15:53:21 +0200

    remove test in api

--- platform/Eventing/DataFusionAPI/src/test/java/eu/linksmart/api/event/datafusion/testing/apiTest.java
+++ platform/Eventing/DataFusionAPI/src/test/java/eu/linksmart/api/event/datafusion/testing/apiTest.java
@@ -1,21 +1,21 @@
 package eu.linksmart.api.event.datafusion.testing;
 
 
-import org.junit.Test;
+//import org.junit.Test;
 
 import java.util.logging.Logger;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 
 public class apiTest {
 
     private Logger mlogger = Logger.getLogger(apiTest.class.getName());
 
-    @Test
+  //  @Test
     public void testSomething() {
         //TODO dummy assertion. Add your proper tests
-        assertTrue(true);
+    //    assertTrue(true);
 
     }
 }
