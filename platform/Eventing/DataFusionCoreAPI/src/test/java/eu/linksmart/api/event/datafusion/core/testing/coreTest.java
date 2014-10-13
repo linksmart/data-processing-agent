6660176af6aecac4664a97f69822bfc5d417f7bf
Jose Angel <carvajal@fit.fhg.de>
2014-10-13 16:21:48 +0200

    remove test code in the APIs

--- platform/Eventing/DataFusionCoreAPI/src/test/java/eu/linksmart/api/event/datafusion/core/testing/coreTest.java
+++ platform/Eventing/DataFusionCoreAPI/src/test/java/eu/linksmart/api/event/datafusion/core/testing/coreTest.java
@@ -1,18 +1,18 @@
 package eu.linksmart.api.event.datafusion.core.testing;
-
+/*
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
-
+*/
 public class coreTest {
 
-    private Logger mlogger = Logger.getLogger(coreTest.class.getName());
+  //  private Logger mlogger = Logger.getLogger(coreTest.class.getName());
 
-    @Test
+    //@Test
     public void testSomething() {
         //TODO dummy assertion. Add your proper tests
-        assertTrue(true);
+      //  assertTrue(true);
 
     }
 }
