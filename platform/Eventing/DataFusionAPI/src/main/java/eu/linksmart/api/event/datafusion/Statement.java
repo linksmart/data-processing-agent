ebe02f73d3b5debfb50d74ed053e2e610318376a
Jose Angel <carvajal@fit.fhg.de>
2014-10-14 16:13:16 +0200

    add Statement.java

---
+++ platform/Eventing/DataFusionAPI/src/main/java/eu/linksmart/api/event/datafusion/Statement.java
@@ -0,0 +1,24 @@
+package eu.linksmart.api.event.datafusion;
+
+/**
+ * Created by Caravajal on 06.10.2014.
+ */
+public interface Statement {
+
+    public String getName();
+
+    public String getQuery();
+
+    public String[] getInput();
+
+    public String[] getScope();
+
+    public String getInput(int index);
+
+    public String getScope(int index);
+
+    public String[] getOutput();
+    public boolean haveInput();
+    public boolean haveOutput();
+    public boolean haveScope();
+}
