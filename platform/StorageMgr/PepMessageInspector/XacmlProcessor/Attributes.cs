7595325079b248cf58eb6dd2e1b9577cf9f71bb7
Mark Vinkovits <mark.vinkovits@fit.fraunhofer.de>
2014-10-16 15:34:24 +0200

    StorageMgr PEP skeleton

---
+++ platform/StorageMgr/PepMessageInspector/XacmlProcessor/Attributes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XacmlProcessor
+{
+    public class Attributes
+    {
+        [XmlIgnore]
+        public static string subjectCategory = "urn:oasis:names:tc:xacml:1.0:subject-category:access-subject";
+        [XmlIgnore]
+        public static string resourceCategory = "urn:oasis:names:tc:xacml:3.0:attribute-category:resource";
+        [XmlIgnore]
+        public static string actionCategory = "urn:oasis:names:tc:xacml:3.0:attribute-category:action";
+
+        [XmlAttribute]
+        public string Category { get; set; }
+
+        [XmlElement]
+        public List<Attribute> Attribute = new List<Attribute>();
+
+        public void addAttribute(Attribute attr)
+        {
+            Attribute.Add(attr);
+        }
+    }
+}
