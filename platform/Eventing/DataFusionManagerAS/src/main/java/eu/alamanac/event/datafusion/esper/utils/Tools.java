112df578fcb97c2fee7b4dd0adb746e8681b7b81
Jose Angel <carvajal@fit.fhg.de>
2014-10-20 09:37:53 +0200

    add high precision  time

--- platform/Eventing/DataFusionManagerAS/src/main/java/eu/alamanac/event/datafusion/esper/utils/Tools.java
+++ platform/Eventing/DataFusionManagerAS/src/main/java/eu/alamanac/event/datafusion/esper/utils/Tools.java
@@ -15,7 +15,7 @@ public class Tools {
 
 
                 TimeZone tz = TimeZone.getTimeZone("UTC");
-                DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+                DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
                 df.setTimeZone(tz);
                 String nowAsISO = df.format(new Date());
 
