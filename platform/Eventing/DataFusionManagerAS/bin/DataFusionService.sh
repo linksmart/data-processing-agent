8027d79dac6abcee90101dc3651837ff0a1c1cda
Jose Angel <carvajal@fit.fhg.de>
2014-10-22 15:21:16 +0200

    add root dir

--- platform/Eventing/DataFusionManagerAS/bin/DataFusionService.sh
+++ platform/Eventing/DataFusionManagerAS/bin/DataFusionService.sh
@@ -8,4 +8,4 @@ ROOT_DIR=$DIR/..
 
 echo "`date`: Started Simulator..."
 
-exec java -jar eu.almanac.event.datafusion.imp-1.0.0-SNAPSHOT-jar-with-dependencies.jar "tcp://130.192.86.227:1883" 
+exec java -jar $ROOT_DIR/eu.almanac.event.datafusion.imp-1.0.0-SNAPSHOT-jar-with-dependencies.jar "tcp://130.192.86.227:1883" 
