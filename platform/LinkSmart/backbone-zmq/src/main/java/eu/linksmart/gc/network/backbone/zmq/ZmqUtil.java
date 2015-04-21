424bae382945359b4fe647bf1dcabbbf6b39af81
Jose Angel <carvajal@fit.fhg.de>
2015-04-21 09:38:50 +0200

    fixing isSync in payload?

--- platform/LinkSmart/backbone-zmq/src/main/java/eu/linksmart/gc/network/backbone/zmq/ZmqUtil.java
+++ platform/LinkSmart/backbone-zmq/src/main/java/eu/linksmart/gc/network/backbone/zmq/ZmqUtil.java
@@ -36,7 +36,8 @@ public class ZmqUtil {
 	public static byte[] addVADsToPayload(BackboneMessage bbMessage) {
 		byte[] payloadWithReceiverVAD = ArrayUtils.addAll(bbMessage.getReceiverVirtualAddress().getBytes(),bbMessage.getPayload());
 		byte[] payloadWithVADs = ArrayUtils.addAll(bbMessage.getSenderVirtualAddress().getBytes(),payloadWithReceiverVAD);
-        byte[] payloadWithReceiverVADiSync = ArrayUtils.addAll(payloadWithVADs,new byte[]{bbMessage.isSync()?(byte)1:(byte)0});
+        byte[] aux = new byte[]{bbMessage.isSync()?(byte)1:(byte)0};
+        byte[] payloadWithReceiverVADiSync = ArrayUtils.addAll(aux,payloadWithVADs);
 		return payloadWithReceiverVADiSync;
 	}
 	
