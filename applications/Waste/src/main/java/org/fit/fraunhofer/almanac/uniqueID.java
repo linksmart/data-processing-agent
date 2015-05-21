0dd3a210212a9827340363a16adbbdee3a9b2c8f
Werner-Kytölä <otilia.werner-kytoelae@fit.fraunhofer.de>
2015-05-21 10:24:27 +0200

    first commit

---
+++ applications/Waste/src/main/java/org/fit/fraunhofer/almanac/uniqueID.java
@@ -0,0 +1,33 @@
+package org.fit.fraunhofer.almanac;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
+/**
+ * Created by Werner-Kytölä on 08.05.2015.
+ */
+public class uniqueID {
+    public static String generateUUID(){
+        try {
+            return sha256(UUID.randomUUID().toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    static String sha256(String input) throws NoSuchAlgorithmException {
+
+        final MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+}
