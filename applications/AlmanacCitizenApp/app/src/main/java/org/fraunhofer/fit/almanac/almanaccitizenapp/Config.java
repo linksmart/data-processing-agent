9242ecac749b5ea98cb3e47454c43a9081c94f5f
devasya <devasya@flash8.fit.fraunhofer.de>
2015-09-01 12:28:30 +0200

    Disable test local

--- applications/AlmanacCitizenApp/app/src/main/java/org/fraunhofer/fit/almanac/almanaccitizenapp/Config.java
+++ applications/AlmanacCitizenApp/app/src/main/java/org/fraunhofer/fit/almanac/almanaccitizenapp/Config.java
@@ -4,5 +4,5 @@ package org.fraunhofer.fit.almanac.almanaccitizenapp;
  * Created by devasya on 27.07.2015.
  */
 public class Config {
-    public static final boolean TESTLOCALLY = true;
+    public static final boolean TESTLOCALLY = false;
 }
