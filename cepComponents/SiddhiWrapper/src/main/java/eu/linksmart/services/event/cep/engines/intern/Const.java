25bc14ea808d79cd176734bc75e2a6e7556227cb
Jose Angel <carvajal@fit.fhg.de>
2017-03-29 17:18:12 +0200

    * Configurator now takes the last conf file for properties * restructuration of the repository * change the POST /statement/add to POST /statement/  * now is possible to dynamic load the external JARs by starting the application: for MQTT "java -cp ./* eu.linksmart.services.Application <<conf.cfg>>"; for REST "java -cp ./* org.springframework.boot.loader.PropertiesLauncher <<conf.cfg>>"  * add unit test for heavy load for the for MessageForwarding class

---
+++ cepComponents/SiddhiWrapper/src/main/java/eu/linksmart/services/event/cep/engines/intern/Const.java
@@ -0,0 +1,9 @@
+package eu.linksmart.services.event.cep.engines.intern;
+
+/**
+ * Created by José Ángel Carvajal on 08.06.2016 a researcher of Fraunhofer FIT.
+ */
+public interface Const extends eu.linksmart.services.utils.constants.Const {
+    public static final String STATEMENT_INOUT_BASE_TOPIC_CONF_PATH ="STATEMENT_INOUT_BASE_TOPIC";
+    public static final String DEFAULT_CONFIGURATION_FILE = "conf.cfg";
+}
