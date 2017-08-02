d2babebc5f3c6a1097915c88114de09e71cad0c0
Jose Angel <carvajal@fit.fhg.de>
2017-08-02 10:51:09 +0200

    Remove will from configuration file

--- DataProcessingCore/src/main/java/eu/linksmart/services/event/intern/Const.java
+++ DataProcessingCore/src/main/java/eu/linksmart/services/event/intern/Const.java
@@ -12,49 +12,44 @@ import eu.linksmart.services.event.handler.HandlerConst;
 public interface Const extends eu.linksmart.services.utils.constants.Const, eu.linksmart.services.event.feeders.Const, HandlerConst {
 
 
-    public static final String STATEMENT_INOUT_BROKER_CONF_PATH ="api_statements_mqtt_broker";
-    public static final String STATEMENT_INOUT_BASE_TOPIC_CONF_PATH ="api_statements_mqtt_topic_base";
-    public static final String STATEMENT_IN_TOPIC_ADD_CONF_PATH ="api_statements_mqtt_topic_base_add";
-    public static final String STATEMENT_IN_TOPIC_CREATE_CONF_PATH ="api_statements_mqtt_topic_base_create";
-    public static final String STATEMENT_IN_TOPIC_DELETE_CONF_PATH ="api_statements_mqtt_topic_base_remove";
-    public static final String STATEMENT_IN_TOPIC_UPDATE_CONF_PATH ="api_statements_mqtt_topic_base_update";
-    public static final String STATEMENT_OUT_TOPIC_ERROR_CONF_PATH ="api_statements_mqtt_topic_base_errors";
+    String STATEMENT_INOUT_BROKER_CONF_PATH ="api_statements_mqtt_broker";
+    String STATEMENT_INOUT_BASE_TOPIC_CONF_PATH ="api_statements_mqtt_topic_base";
+    String STATEMENT_IN_TOPIC_ADD_CONF_PATH ="api_statements_mqtt_topic_base_add";
+    String STATEMENT_IN_TOPIC_CREATE_CONF_PATH ="api_statements_mqtt_topic_base_create";
+    String STATEMENT_IN_TOPIC_DELETE_CONF_PATH ="api_statements_mqtt_topic_base_remove";
+    String STATEMENT_IN_TOPIC_UPDATE_CONF_PATH ="api_statements_mqtt_topic_base_update";
+    String STATEMENT_OUT_TOPIC_ERROR_CONF_PATH ="api_statements_mqtt_topic_base_errors";
+    String EVENT_IN_TOPIC_CONF_PATH = "api_events_mqtt_topic_incoming";
+    String EVENTS_IN_BROKER_CONF_PATH ="api_events_mqtt_broker_incoming";
+    String LOG_HEARTBEAT_TIME_CONF_PATH = "core_monitoring_heartbeat_every_milliseconds";
 
-    public static final String EVENT_IN_TOPIC_CONF_PATH = "api_events_mqtt_topic_incoming";
-    public static final String EVENTS_IN_BROKER_CONF_PATH ="api_events_mqtt_broker_incoming";
+    String PERSISTENT_DATA_FILE = "cep_init_bootstrapping_files";
 
-    public static final String LOG_HEARTBEAT_TIME_CONF_PATH = "core_monitoring_heartbeat_every_milliseconds";
+    String PERSISTENT_EVENTS_FILE = "cep_init_bootstrapping_events_files";
 
-    public static final String PERSISTENT_DATA_FILE = "cep_init_bootstrapping_files";
+    String CEP_ENGINES_PATH = "cep_init_engines";
 
-    public static final String PERSISTENT_EVENTS_FILE = "cep_init_bootstrapping_events_files";
-    public static final String PERSISTENT_DATA_DIRECTORY = "cep_init_bootstrapping_directories";
+    String DEFAULT_CONFIGURATION_FILE = "__def__conf__.cfg";
 
-    public static final String CEP_ENGINES_PATH = "cep_init_engines";
+    String APPLICATION_CONFIGURATION_FILE = "conf.cfg";
+    String ID_CONF_PATH = "agent_id";
 
-    public static final String DEFAULT_CONFIGURATION_FILE = "__def__conf__.cfg";
-
-    public static final String APPLICATION_CONFIGURATION_FILE = "conf.cfg";
-    public static final String ID_CONF_PATH = "agent_id";
-
-    public static final String FeederPayloadClass ="connector_observers_payload_type_class";
-    public static final String FeederPayloadAlias ="connector_observers_type_aliases";
+    String FeederPayloadClass ="connector_observers_payload_type_class";
+    String FeederPayloadAlias ="connector_observers_type_aliases";
 
 
-    public static final String AdditionalImportPackage ="cep_init_additionalImportPackage";
+    String AdditionalImportPackage ="cep_init_additionalImportPackage";
 
-    public static final String ADDITIONAL_CLASS_TO_BOOTSTRAPPING = "agent_init_extensions";
+    String ADDITIONAL_CLASS_TO_BOOTSTRAPPING = "agent_init_extensions";
 
-    public static final String START_MQTT_STATEMENT_API = "api_statements_mqtt_enable";
-    public static final String WILL_TOPIC = "api_statements_mqtt_topic_base_will";
-    public static final String WILL_MESSAGE = "api_statements_mqtt_message_base_will";
-    public static final String MONITOR_TOPICS = "connector_monitoring_mqtt_events_report_topics";
-    public static final String MONITOR_EVENTS = "connector_monitoring_mqtt_events_report_everySeconds";
-    public static final String ENVIRONMENTAL_VARIABLES_ENABLED = "env_var_enabled";
-    public static final String ENABLE_REST_API = "api_rest_enabled";
-    public static final String SPRING_MANAGED_FEATURES = "spring_managed_configuration_features";
+    String START_MQTT_STATEMENT_API = "api_statements_mqtt_enable";
+    String MONITOR_TOPICS = "connector_monitoring_mqtt_events_report_topics";
+    String MONITOR_EVENTS = "connector_monitoring_mqtt_events_report_everySeconds";
+    String ENVIRONMENTAL_VARIABLES_ENABLED = "env_var_enabled";
+    String ENABLE_REST_API = "api_rest_enabled";
+    String SPRING_MANAGED_FEATURES = "spring_managed_configuration_features";
 
-    public static final String REST_API_EXTENSION = "rest_api_extensions";
+    String REST_API_EXTENSION = "rest_api_extensions";
     String AGENT_DESCRIPTION = "agent_description";
     String REGISTRATION_TOPIC = "registration_topic";
     String REGISTRATION_TOPIC_WILL = "topic_will";
