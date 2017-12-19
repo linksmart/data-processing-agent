e397e5b1259146dd7715e5840c5c5b0a2d1d5020
Jose Angel <carvajal@fit.fhg.de>
2017-12-19 15:27:21 +0100

    remove agent utils

--- Utils/src/main/java/eu/linksmart/services/utils/logging/LoggerServiceConst.java
+++
@@ -1,9 +0,0 @@
-package eu.linksmart.services.utils.logging;
-
-/**
- * Created by José Ángel Carvajal on 07.08.2015 a researcher of Fraunhofer FIT.
- */
-public interface LoggerServiceConst {
-
-    public static final String LoggingDefaultLoggingFile  ="logging.default.configuration.file";
-}
--- Utils/src/main/java/eu/linksmart/services/utils/serialization/DeserializerMode.java
+++
@@ -1,9 +0,0 @@
-package eu.linksmart.services.utils.serialization;
-
-import com.fasterxml.jackson.databind.JsonDeserializer;
-
-/**
- * Created by José Ángel Carvajal on 27.10.2017 a researcher of Fraunhofer FIT.
- */
-public abstract class DeserializerMode<T> extends JsonDeserializer<T> {
-}
