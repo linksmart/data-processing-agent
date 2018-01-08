6eb5c5f68a2c577d1ebfc465311f9e303bfc9f64
Jose Angel <carvajal@fit.fhg.de>
2018-01-08 14:29:43 +0100

    enable service discovery

--- cepComponents/EsperWrapper/src/main/java/eu/linksmart/services/event/cep/engines/intern/Const.java
+++ cepComponents/EsperWrapper/src/main/java/eu/linksmart/services/event/cep/engines/intern/Const.java
@@ -5,10 +5,10 @@ package eu.linksmart.services.event.cep.engines.intern;
  */
 public interface Const extends eu.linksmart.services.utils.constants.Const {
 
-    public static final String DEFAULT_CONFIGURATION_FILE = "__def__conf__.cfg";
-    public static final String SIMULATION_EXTERNAL_CLOCK = "cep_simulation_externalClock";
+    String DEFAULT_CONFIGURATION_FILE = "__def__conf__.cfg";
+    String SIMULATION_EXTERNAL_CLOCK = "cep_simulation_externalClock";
 
-    public static final String SIMULATION_EXTERNAL_CLOCK_STARTING_TIME = "cep_simulation_staringTime";
+    String SIMULATION_EXTERNAL_CLOCK_STARTING_TIME = "cep_simulation_staringTime";
 
 
 }
