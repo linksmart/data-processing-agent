2f9cf2d274da89f36eb9a6facbadddd0916cbfd3
Jose Angel <carvajal@fit.fhg.de>
2018-06-13 12:07:24 +0200

    id and time are sent in the statement to the complex event handler

--- DataProcessingCore/src/main/java/eu/linksmart/services/event/handler/ComplexEventHandler.java
+++ DataProcessingCore/src/main/java/eu/linksmart/services/event/handler/ComplexEventHandler.java
@@ -7,7 +7,6 @@ import eu.linksmart.services.event.handler.base.BaseMapEventHandler;
 import eu.linksmart.services.event.intern.Const;
 import eu.linksmart.services.event.intern.SharedSettings;
 import eu.linksmart.api.event.components.ComplexEventPropagationHandler;
-import eu.linksmart.api.event.components.Enveloper;
 import eu.linksmart.api.event.components.Publisher;
 import eu.linksmart.services.utils.mqtt.broker.BrokerConfiguration;
 import eu.linksmart.services.utils.serialization.Serializer;
@@ -84,34 +83,14 @@ import java.util.stream.Collectors;
                 Arrays.stream(eventMap).forEach(i->applyMap(i,aux));
                 processSingleMap(aux);
             }else {
-
-                try {
-                    query.setLastOutput(
-                            builder.factory(
-                                    SharedSettings.getId(),
-                                    query.getId(),
-                                    eventMap,
-                                    new Date(),
-                                    null,
-                                    new HashMap<>()
-                            )
-                    );
-                } catch (UntraceableException e) {
-
-                    loggerService.error(e.getMessage(), e);
-                }
-
-                try {
-                    publisher.publish(serializer.serialize(query.getLastOutput()));
-                } catch (Exception e) {
-                    loggerService.error(e.getMessage(), e);
-                }
+                packLastOutput( SharedSettings.getId(),query.getId(),eventMap,(new Date()).getTime(),null,new HashMap<>());
+                publishLastOutput();
             }
         }
 
 
     }
-    private void applyMap(Map source,Map<Object,Collection> target){
+    private void applyMap(Map source,Map<Object,Collection> target){ // repack the map using the k/key as keys and the v/values as values
         target.putIfAbsent(source.getOrDefault("k",source.get("key")), new ArrayList());
         if(source.getOrDefault("v",source.get("values")) instanceof Collection)
             target.get(source.getOrDefault("k",source.get("key"))).addAll(  (Collection) source.getOrDefault("v",source.get("values")) );
@@ -125,73 +104,46 @@ import java.util.stream.Collectors;
     protected void processSingleMap(Map eventMap){
         if(builder!=null) {
             if (eventMap.size() == 1 && eventMap.values().toArray()[0]!=null) { // has only one element
-                try {
-                    query.setLastOutput(builder.factory(
-                            SharedSettings.getId(),
-                            query.getId(),
-                            eventMap.values().toArray()[0],
-                            (new Date()).getTime(),
-                            null,
-                            new HashMap<>()
-                    ));
-                } catch (UntraceableException e) {
-                    loggerService.error(e.getMessage(), e);
-                }
-                // if the eventMap is only one then is sent as one event
-                try {
-                    publisher.publish(serializer.serialize(query.getLastOutput()));
 
-                } catch (Exception eEntity) {
-                    loggerService.error(eEntity.getMessage(), eEntity);
-                }
+                packLastOutput( SharedSettings.getId(),query.getId(),eventMap.values().toArray()[0],(new Date()).getTime(),null,new HashMap<>());
+                // if the eventMap is only one then is sent as one event
+                publishLastOutput();
+
             }  else if (eventMap.size() == 2 &&( eventMap.containsKey("k") && eventMap.get("k")!= null || eventMap.containsKey("key") && eventMap.get("key")!= null) && (eventMap.containsKey("v") && eventMap.get("v")!= null || eventMap.containsKey("values") && eventMap.get("values")!= null)) {
                 // re-creates the map mapping the given keys and values
                 final Map aux = new HashMap();
                 applyMap(eventMap,aux);
                 processSingleMap(aux);
             }else if (!eventMap.isEmpty()){ // if there is objects to work with
-                Object tmpDate = eventMap.getOrDefault("time", eventMap.getOrDefault("Time", eventMap.getOrDefault("date", eventMap.getOrDefault("Date", new Date()))));
-                Date date = ((tmpDate instanceof Date) ? (Date) tmpDate : ((tmpDate instanceof Long) ? new Date((Long) tmpDate) : new Date()));
-                try {
-                    query.setLastOutput(
-                            builder.factory(
-                                    SharedSettings.getId(),
-                                    query.getId(),
-                                    eventMap,
-                                    date.getTime(),
-                                    null,
-                                    new HashMap<>()
-                            )
-                    );
-                } catch (UntraceableException e) {
+                Object tmpDate = eventMap.getOrDefault("time", eventMap.getOrDefault("Time", eventMap.getOrDefault("date", eventMap.getOrDefault("Date", null))));
+                Object tmpId = eventMap.getOrDefault("id", eventMap.getOrDefault("ID", eventMap.getOrDefault("Id", eventMap.getOrDefault("@iot.id", eventMap.getOrDefault("datastream.id", null)))));
 
-                    loggerService.error(e.getMessage(), e);
+                if((tmpDate!= null && tmpId!= null && eventMap.size()==3)|| ((tmpDate!= null || tmpId!= null) && eventMap.size()==2)) { // id the Id or time of the Datastream is defined
+                    Date date = (tmpDate == null) ? new Date() : ((tmpDate instanceof Date) ? (Date) tmpDate : ((tmpDate instanceof Long) ? new Date((Long) tmpDate) : new Date()));
+                    if (eventMap.size() == 3) { // time and id are given
+                        eventMap.remove(tmpDate);
+                        eventMap.remove(tmpId);
+                        packLastOutput( SharedSettings.getId(),tmpId,eventMap.values().toArray()[0],date.getTime(),null,new HashMap<>());
+                    } else if (tmpDate != null) { // time is given
+                        eventMap.remove(tmpDate);
+                        eventMap.remove(tmpId);
+                        packLastOutput( SharedSettings.getId(),tmpId,eventMap.values().toArray()[0],date.getTime(),null,new HashMap<>());
+                    } else { // if Id is given
+                        eventMap.remove(tmpId);
+                        packLastOutput( SharedSettings.getId(),tmpId,eventMap.values().toArray()[0],date.getTime(),null,new HashMap<>());
+                    }
+                }else { // if none are given
+                    Date date = (tmpDate == null) ? new Date() : ((tmpDate instanceof Date) ? (Date) tmpDate : ((tmpDate instanceof Long) ? new Date((Long) tmpDate) : new Date()));
+
+                    packLastOutput(SharedSettings.getId(), query.getId(), eventMap, date.getTime(), null, new HashMap<>());
                 }
 
                 // if the eventMap has several events in it
                 if (conf.getBoolean(Const.AGGREGATE_EVENTS_CONF)) {
                     // if the aggregation option is on; the whole map is send as it is
-                    try {
-                        publisher.publish(serializer.serialize(query.getLastOutput()));
-                    } catch (Exception e) {
-                        loggerService.error(e.getMessage(), e);
-                    }
+                    publishLastOutput();
                 } else {
-                    try {
-                        query.setLastOutput(
-                                builder.factory(
-                                        SharedSettings.getId(),
-                                        query.getId(),
-                                        eventMap,
-                                        date.getTime(),
-                                        null,
-                                        new HashMap<>()
-                                )
-                        );
-                    } catch (UntraceableException e) {
 
-                        loggerService.error(e.getMessage(), e);
-                    }
                     // if the aggregation option is off; each value of the map is send as an independent event
 
                     eventMap.keySet().forEach(key -> {
@@ -204,20 +156,32 @@ import java.util.stream.Collectors;
                     );
                 }
             }
-        }else {
+        }else { // if there no event builder set
 
             query.setLastOutput(eventMap);
 
             // if the eventMap is only one then is sent as one event
-            try {
-                publisher.publish(serializer.serialize(query.getLastOutput()));
-
-            } catch (Exception eEntity) {
-                loggerService.error(eEntity.getMessage(), eEntity);
-            }
+           publishLastOutput();
         }
     }
 
+    private void packLastOutput(Object thingId, Object streamID, Object value, long date, String url, Map<String, Object> map){
+        try {
+            query.setLastOutput(
+                    builder.factory(thingId,streamID,value,date,url,map)
+            );
+        } catch (UntraceableException e) {
+
+            loggerService.error(e.getMessage(), e);
+        }
+    }
+    private void publishLastOutput(){
+        try {
+            publisher.publish(serializer.serialize(query.getLastOutput()));
+        } catch (Exception eEntity) {
+            loggerService.error(eEntity.getMessage(), eEntity);
+        }
+    }
     @Override
     public  synchronized void destroy(){
 
