75aedc3f3750618e49d92bf7e7473d0c44aa6985
Jannis Warnat <jannis.warnat@fit.fraunhofer.de>
2018-07-13 13:23:40 +0200

    LS-414 IoT agents persistence file is growing with every restart
    Check if ArrayList already contains the encoded String before writing to file

--- DataProcessingCore/src/main/java/eu/linksmart/services/event/types/PersistentRequestInstance.java
+++ DataProcessingCore/src/main/java/eu/linksmart/services/event/types/PersistentRequestInstance.java
@@ -18,8 +18,8 @@ import java.util.*;
  * Created by José Ángel Carvajal on 30.10.2017 a researcher of Fraunhofer FIT.
  */
 public abstract class PersistentRequestInstance implements PersistentRequest {
-    protected static final transient  Logger loggerService = LogManager.getLogger(PersistentRequestInstance.class);
-    protected static final transient  Configurator conf =  Configurator.getDefaultConfig();
+    protected static final transient Logger loggerService = LogManager.getLogger(PersistentRequestInstance.class);
+    protected static final transient Configurator conf = Configurator.getDefaultConfig();
     private static final transient Timer persistentRequestTracker = new Timer();
     private static final transient List<PersistentRequestInstance> requests = new ArrayList<>();
 
@@ -28,7 +28,7 @@ public abstract class PersistentRequestInstance implements PersistentRequest {
         return persistentFile;
     }
 
-    private static final transient String persistentFile = conf.containsKeyAnywhere(Const.CONNECTOR_PERSISTENT_FILE)?conf.getString(Const.CONNECTOR_PERSISTENT_FILE)+"-"+ SharedSettings.getId()+".json":null;
+    private static final transient String persistentFile = conf.containsKeyAnywhere(Const.CONNECTOR_PERSISTENT_FILE) ? conf.getString(Const.CONNECTOR_PERSISTENT_FILE) + "-" + SharedSettings.getId() + ".json" : null;
 
 
     @JsonProperty("persistent")
@@ -53,6 +53,7 @@ public abstract class PersistentRequestInstance implements PersistentRequest {
     public boolean isPersistent() {
         return persistent;
     }
+
     @JsonProperty("essential")
     @ApiModelProperty(notes = "Indicates if the request fails to be loaded the service should be crash or not. Note: if the agent is not configured to crash, it will not crash regardless of this setting")
     @Override
@@ -66,20 +67,22 @@ public abstract class PersistentRequestInstance implements PersistentRequest {
     public void isPersistent(boolean persistent) {
         this.persistent = persistent;
     }
+
     public String getId() {
-        if(id==null||id.equals(""))
+        if (id == null || id.equals(""))
             id = eu.linksmart.services.utils.function.Utils.hashIt((new Date()).toString());
         return id;
     }
 
-    public void setId(String id){
-        this.id =id;
+    public void setId(String id) {
+        this.id = id;
     }
+
     @JsonProperty("essential")
     @ApiModelProperty(notes = "Indicates if the request fails to be loaded the service should be crash or not. Note: if the agent is not configured to crash, it will not crash regardless of this setting")
     @Override
     public void isEssential(boolean essential) {
-        if(     !conf.getBoolean(Const.PERSISTENT_ENABLED) &&
+        if (!conf.getBoolean(Const.PERSISTENT_ENABLED) &&
                 !conf.getBoolean(Const.FAIL_IF_PERSISTENCE_FAILS) &&
                 essential
                 ) {
@@ -89,24 +92,28 @@ public abstract class PersistentRequestInstance implements PersistentRequest {
         this.essential = essential;
     }
 
-      static {
-        if(persistentFile!=null){
+    static {
+        if (persistentFile != null) {
 
             persistentRequestTracker.schedule(
                     new TimerTask() {
                         @Override
                         public void run() {
-                            synchronized (persistentFile){
+                            synchronized (persistentFile) {
                                 OutputStream outputStream = null;
                                 try {
                                     Map<String, List<String>> toStore = new HashMap<>();
-                                    requests.stream().filter(p->p.persistent).forEach(r -> {
+                                    requests.stream().filter(p -> p.persistent).forEach(r -> {
 
                                         try {
                                             if (!toStore.containsKey(r.getClass().getCanonicalName()))
                                                 toStore.put(r.getClass().getCanonicalName(), new ArrayList<>());
 
-                                            toStore.get(r.getClass().getCanonicalName()).add(Base64.getEncoder().encodeToString(SharedSettings.getSerializer().serialize(r)));
+                                            String serialized = Base64.getEncoder().encodeToString(SharedSettings.getSerializer().serialize(r));
+
+                                            if (!toStore.get(r.getClass().getCanonicalName()).contains(serialized)) {
+                                                toStore.get(r.getClass().getCanonicalName()).add(serialized);
+                                            }
                                         } catch (Exception e) {
                                             loggerService.error(e.getMessage(), e);
                                             System.exit(-1);
@@ -124,12 +131,13 @@ public abstract class PersistentRequestInstance implements PersistentRequest {
                                         outputStream.close();
                                     }
                                 } catch (IOException e) {
-                                   loggerService.error(e.getMessage(),e);
-                                }finally {
+                                    loggerService.error(e.getMessage(), e);
+                                } finally {
                                     try {
-                                        if(outputStream!=null)
+                                        if (outputStream != null)
                                             outputStream.close();
-                                    } catch (Exception ignored) {}
+                                    } catch (Exception ignored) {
+                                    }
                                 }
                             }
 
