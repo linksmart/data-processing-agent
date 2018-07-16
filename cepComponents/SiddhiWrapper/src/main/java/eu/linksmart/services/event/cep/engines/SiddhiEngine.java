37c98933b7afce8f84f6dfccef396cdcee245f02
Jannis Warnat <jannis.warnat@fit.fraunhofer.de>
2018-07-16 16:24:54 +0200

    Revert "LS-384 Update statements with PUT requests"
    This reverts commit f9e8887bcec6ca531e8935821c52f54170641b36.

--- cepComponents/SiddhiWrapper/src/main/java/eu/linksmart/services/event/cep/engines/SiddhiEngine.java
+++ cepComponents/SiddhiWrapper/src/main/java/eu/linksmart/services/event/cep/engines/SiddhiEngine.java
@@ -204,13 +204,6 @@ public class SiddhiEngine extends Component implements CEPEngine {
     }
 
     @Override
-    public boolean updateStatement(Statement statement)  {
-
-        loggerService.error("updateStatement(String id, Statement statement) function not implemented");
-        return false;
-    }
-
-    @Override
     public boolean pauseStatement(String id) throws StatementException {
 
         loggerService.error("pauseStatement(String id) function not implemented");
