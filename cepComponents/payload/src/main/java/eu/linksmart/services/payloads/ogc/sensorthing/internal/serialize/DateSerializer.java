93dc5a37b47cbe1f2a127fd955254bf81334b18c
Jannis Warnat <jannis.warnat@fit.fraunhofer.de>
2018-07-23 13:02:11 +0200

    switch DateFormat to isoFormatMSWTZ so GOST understands

--- cepComponents/payload/src/main/java/eu/linksmart/services/payloads/ogc/sensorthing/internal/serialize/DateSerializer.java
+++ cepComponents/payload/src/main/java/eu/linksmart/services/payloads/ogc/sensorthing/internal/serialize/DateSerializer.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
+import java.text.DateFormat;
+import eu.linksmart.services.utils.function.Utils;
 
 import java.io.IOException;
 import java.util.Date;
 
 public class DateSerializer extends JsonSerializer<Date> {
-    private static StdDateFormat formatter = new StdDateFormat();
+    private static DateFormat formatter = Utils.isoFormatMSWTZ;
 
     public DateSerializer() {
         super();
