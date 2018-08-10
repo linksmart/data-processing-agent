09944af27a293a99803fe075ac9c44d2eda480d9
Jannis Warnat <jannis.warnat@fit.fraunhofer.de>
2018-08-10 15:29:03 +0200

    respect setting general_time_zone in DateSerializer for OGC

--- cepComponents/payload/src/main/java/eu/linksmart/services/payloads/ogc/sensorthing/internal/serialize/DateSerializer.java
+++ cepComponents/payload/src/main/java/eu/linksmart/services/payloads/ogc/sensorthing/internal/serialize/DateSerializer.java
@@ -20,6 +20,7 @@ public class DateSerializer extends JsonSerializer<Date> {
 
     @Override
     public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        formatter.setTimeZone(Utils.getTimeZone());
         jsonGenerator.writeString(formatter.format(date));
     }
 }
