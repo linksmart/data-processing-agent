34832d5e05e6a9b307e1bd82f456ae0fd2d703ba
Jose Angel <carvajal@fit.fhg.de>
2019-08-08 09:13:54 +0200

    configure linksmart as maven central

--- cemlComponents/models/DummyClassifier/src/main/java/eu/linksmart/services/event/ceml/models/DummyClassifier.java
+++ cemlComponents/models/DummyClassifier/src/main/java/eu/linksmart/services/event/ceml/models/DummyClassifier.java
@@ -19,23 +19,45 @@ import java.util.function.Function;
  * Created by José Ángel Carvajal on 05.10.2018 a researcher of Fraunhofer FIT.
  */
 public class DummyClassifier extends ClassifierModel<List<Number>,Number,Function<List<Number>,Integer>> {
+
+
+    private long printEvery = 10, counterLearn = 0, counterPredict = 0;
+
     public DummyClassifier(List<TargetRequest> targets, Map<String, Object> parameters, Object learner) {
         super(targets, parameters, learner);
-        this.learner = numbers -> numbers.stream().mapToInt(i->((Number)i).intValue()).sum() % 2;
+        this.learner = numbers -> numbers.stream().mapToInt(i -> ((Number) i).intValue()).sum() % 2;
     }
+
     final static Logger loggerService = LogManager.getLogger(DummyClassifier.class);
+
     @Override
     public void learn(List<Number> numbers) throws TraceableException, UntraceableException {
-        loggerService.info("learning input arrives ");
-        loggerService.info(numbers);
+        if (Math.floorMod(counterLearn, printEvery) == 0) {
+            loggerService.info("learning input arrives ");
+            loggerService.info(numbers);
+            //counterLearn=0;
+        }
+        counterLearn++;
     }
 
     @Override
     public Prediction<Number> predict(List<Number> numbers) throws TraceableException, UntraceableException {
-        loggerService.info("predict input arrives ");
-        loggerService.info(numbers);
+        if (Math.floorMod(counterPredict, printEvery) == 0) {
+            loggerService.info("predict input arrives ");
+            loggerService.info(numbers);
+            //counterPredict = 0;
+        }
         Collection<EvaluationMetric> evaluationMetrics = new ArrayList<>();
         evaluationMetrics.addAll(evaluator.getEvaluationAlgorithms().values());
-        return new PredictionInstance<>( learner.apply(numbers),numbers, this.getName(), evaluationMetrics);
+        counterPredict++;
+        return new PredictionInstance<>(learner.apply(numbers), numbers, this.getName(), evaluationMetrics);
+    }
+
+    public void setPrintEvery(long printEvery) {
+        this.printEvery = printEvery;
+    }
+
+    public long getPrintEvery() {
+        return printEvery;
     }
 }
